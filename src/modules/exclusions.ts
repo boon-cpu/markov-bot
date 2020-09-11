8825d235daa87119386d0ba55fa4391dd95fb52b
Boon <harrisonboon@gmail.com>
2020-09-11 22:58:52 +0100

    add include all command

--- src/modules/exclusions.ts
+++ src/modules/exclusions.ts
@@ -71,4 +71,28 @@ export default class Exclusions extends Module {
 
     await message.channel.send(`Cheers! now including <@${member.id}>`);
   }
+  @command({
+    aliases: ["incall", "incAll", "includeall"],
+    description: "Removes all users from exclude list",
+  })
+  async includeAll(message: Message) {
+    if (!message.guild) return;
+
+    if (!message.member?.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
+      return message.reply("you do not have permission to use this command!");
+    }
+
+    const server = await Server.findOne({ id: message.guild.id });
+
+    if (!server) {
+      await message.reply("somethign went wrong");
+      return;
+    }
+
+    server.exclude = [];
+
+    await server.save();
+
+    await message.channel.send(`Cheers! now including all members`);
+  }
 }
