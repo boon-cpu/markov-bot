a7e4a11da32b97ec95ab6a2e38b2c58c0440426e
Boon <harrisonboon@gmail.com>
2020-09-11 20:44:28 +0100

    added invite module

---
+++ src/modules/invite.ts
@@ -0,0 +1,19 @@
+import { Message } from "discord.js";
+import { command, default as CookiecordClient, Module } from "cookiecord";
+import { ngram } from "../utils";
+
+export default class Invite extends Module {
+  constructor(client: CookiecordClient) {
+    super(client);
+  }
+
+  @command({
+    aliases: ["inv"],
+    description: "Sends the invite link for the bot",
+  })
+  async invite(message: Message) {
+    message.channel.send(
+      "<https://discord.com/api/oauth2/authorize?client_id=748916794995114035&permissions=0&scope=bot>"
+    );
+  }
+}
